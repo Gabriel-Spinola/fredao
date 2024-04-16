a9dd12cf7f21e32d51f4b91a8bc28c80d4f65436
Gabriel-Spinola <gabrielspinola77@gmail.com>
2024-04-16 17:02:46 -0300

    TOASTS

---
+++ client/scripts/toasts.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ * @param {string} message 
+ * @param {'success' | 'failure'} status 
+ * @param {void | Promise<void>} callback 
+ */
+export function showToast(message, status, callback) {}
